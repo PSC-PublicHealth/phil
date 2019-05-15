66121dff215e60963dded5894561fbdb88bf7399
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2019-05-15 12:38:40 -0400

    Improvements to vaccinomics script for reproducibility

--- batch_scripts/vaccinomics.sh
+++ batch_scripts/vaccinomics.sh
@@ -13,34 +13,38 @@ export OMP_NUM_THREADS=1
 results_dir=${input_dir}/trans-${trans}_efficacy-${efficacy}
 mkdir ${results_dir}
 
-cp ${input_dir}/params.vaccinomics ${results_dir}/params.vaccinomics
-cp ${input_dir}/primary_cases_vaccinomics.txt ${results_dir}/primary_cases_vaccinomics.txt
-
-cd ${results_dir}
-
 for i in {1..30};
 do
   day_of_week=$(shuf -i 0-6 -n 1)
   seed=$RANDOM
   realization_number=${i}
 
+  # Create the output directory and move there
+  output_dir=${results_dir}/${realization_number}
+  mkdir ${output_dir}
+  cd ${output_dir}
+
+  # Make a copy of the template files
+  cp ${input_dir}/params.vaccinomics ${output_dir}/params.vaccinomics
+  cp ${input_dir}/primary_cases_vaccinomics.txt ${output_dir}/primary_cases_vaccinomics.txt
+
   # Set the start date
-  if [ ${day_of_week} -eq 0 ]
+  if [[ ${day_of_week} -eq 0 ]]
   then
     sed -i 's/start_date = .*/start_date = 2012-01-01/' params.vaccinomics
-  elif [ ${day_of_week} -eq 1 ]
+  elif [[ ${day_of_week} -eq 1 ]]
   then
     sed -i 's/start_date = .*/start_date = 2012-01-02/' params.vaccinomics
-  elif [ ${day_of_week} -eq 2 ]
+  elif [[ ${day_of_week} -eq 2 ]]
   then
     sed -i 's/start_date = .*/start_date = 2012-01-03/' params.vaccinomics
-  elif [ ${day_of_week} -eq 3 ]
+  elif [[ ${day_of_week} -eq 3 ]]
   then
     sed -i 's/start_date = .*/start_date = 2012-01-04/' params.vaccinomics
-  elif [ ${day_of_week} -eq 4 ]
+  elif [[ ${day_of_week} -eq 4 ]]
   then
     sed -i 's/start_date = .*/start_date = 2012-01-05/' params.vaccinomics
-  elif [ ${day_of_week} -eq 5 ]
+  elif [[ ${day_of_week} -eq 5 ]]
   then
     sed -i 's/start_date = .*/start_date = 2012-01-06/' params.vaccinomics
   else
@@ -58,9 +62,4 @@ do
 
   # Run PHIL
   ${PHIL_HOME}/bin/phil params.vaccinomics
-
-  # Create the output directory and move the results there
-  mkdir ${results_dir}/${realization_number}
-  mv ${results_dir}/OUT ${results_dir}/${realization_number}/
-
 done
